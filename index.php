cd25eb392a078fa93b8f155f5d75fbfcabddb321
Daniel Cordeiro <danielscordeiro7@gmail.com>
2023-02-03 19:23:02 -0300

    criado a parte Javascript para gerar a imagem

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
   <title>Criar imagem a partir de um HTML e salvar no disco com PHP</title>
 </head>
 <body>
 
+  <!-- CONTEÚDO DA IMAGEM -->
+  <div class="capture">
+    <p>Hello world!</p>
+  </div>
 
+  <!-- GERAR A IMAGEM EM BASE64 -->
+  <script type="text/javascript" src="html2canvas.min.js"></script>
+  <script>
+    html2canvas(document.querySelector(".capture"))
+    .then(canvas => {
+      let img = canvas.toDataURL()
+      console.log(img)
+    });
+  </script>
 
 </body>
 </html>
